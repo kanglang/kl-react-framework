d03e4924b00c6123c36394bf38c00f8147ab29d4
langlang.kang <langlang.kang@guojutech.net>
2021-05-06 16:30:11 +0800

    update

--- src/pages/mine/index.js
+++ src/pages/mine/index.js
@@ -2,7 +2,7 @@
  * @Author: kanglang
  * @Date: 2020-07-14 08:57:14
  * @LastEditors: kanglang
- * @LastEditTime: 2021-05-06 14:34:28
+ * @LastEditTime: 2021-05-06 16:21:02
  * @Description: 我的
  */
 import React, { Component } from 'react';
@@ -13,6 +13,11 @@ class Mine extends Component {
   goHome = () => {
     this.props.history.push('/')
   }
+  
+  goNearby=()=>{
+    this.props.history.push('/Nearby')
+  }
+
   render() {
     return <div style={{ height: '100%', width: '100%', background: '#d6f7d6' }}>
       <div style={{ height: '200px', fontSize: '1.4rem', color: 'pink', padding: '30px 0', textAlign: 'center' }}>我的</div>
@@ -20,6 +25,11 @@ class Mine extends Component {
         btnText="立即体验"
         onClick={this.goHome}
         btnBgColor={'red'}
+      />
+      <SubmitButton
+        btnText="去附近"
+        onClick={this.goNearby}
+        btnBgColor={'blue'}
       />
     </div>
   }
--- src/pages/nearby/index.js
+++ src/pages/nearby/index.js
@@ -1,13 +1,28 @@
+/*
+ * @Author: kanglang
+ * @Date: 2020-07-14 08:57:21
+ * @LastEditors: kanglang
+ * @LastEditTime: 2021-05-06 16:28:30
+ * @Description: 
+ */
 import React, { Component } from 'react';
-
+import { SubmitButton } from '@common'
 
 class Nearby extends Component {
 
-  render () {
+  render() {
     return <div>
-      <button onClick={() => {this.props.history.push('/Mine')}}>我的</button>
-      <button onClick={() => {this.props.history.push('/')}}>首页</button>
-      <div>我是附近</div>
+      <div style={{ height: '100%', width: '100%', background: '#d6f7d6' ,textAlign:'center'}}>我是附近</div>
+      <SubmitButton
+        btnText="我的"
+        onClick={() => { this.props.history.push('/Mine') }}
+        btnBgColor={'red'}
+      />
+      <SubmitButton
+        btnText="首页"
+        onClick={() => { this.props.history.push('/') }}
+        btnBgColor={'blue'}
+      />
     </div>
   }
 
